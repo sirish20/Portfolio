b813c87b615409a988018dc1f7fdacdda687780f
unknown <shubhamsangale20032003@gmail.com>
2024-03-30 01:53:39 +0530

    Changes made

--- app/About/page.js
+++ app/About/page.js
@@ -7,14 +7,14 @@ const About=()=>{
     <div className="px-10 py-8">
      <h1 className="text-3xl  px-10 font-semibold">Current Skill Pursuits</h1>
      <h2 className="px-[10%] mt-[3%] ">As a lifelong learner, I believe in continuously enhancing my skills and staying abreast of the latest developments in the tech industry. Currently, my focus revolves around two key areas:
-     <br></br><br></br><span className="font-semibold">Backend Technologies: </span>I'm currently understanding the backend, the engine that powers the digital experiences we enjoy. Right now, I'm learning about MongoDB (database) , Express.js and Node.js. It's like being the architect of the digital world, building the foundation for great user experiences.
-     <br></br><br></br><span className="font-semibold">Data Structure and Algorithms: </span>I'm working on shrethening my problem solving skills and algorithmic thinking. I'm doing this by learning core data structures and standard algorithms and by participating in coding competitions.
+     <br></br><br></br><span className="font-semibold">Backend Technologies: </span>Im currently understanding the backend, the engine that powers the digital experiences we enjoy. Right now, Im learning about MongoDB (database) , Express.js and Node.js. Its like being the architect of the digital world, building the foundation for great user experiences.
+     <br></br><br></br><span className="font-semibold">Data Structure and Algorithms: </span>Im working on shrethening my problem solving skills and algorithmic thinking. Im doing this by learning core data structures and standard algorithms and by participating in coding competitions.
      </h2>
     </div>
 
     <div className="px-10 pt-8 ">
      <h1 className="text-3xl  px-10 font-semibold">My Vision</h1>
-     <h2 className="px-[10%] mt-[1%] ">"My vision in life is to embrace growth and challenge, while in software engineering, I aim to excel as a versatile professional. I aspire to merge my expertise in DSA, competitive coding, and web development with emerging fields like machine learning and AI. Through continuous learning and innovation, I seek to make a meaningful impact on both the industry and society."
+     <h2 className="px-[10%] mt-[1%] ">My vision in life is to embrace growth and challenge, while in software engineering, I aim to excel as a versatile professional. I aspire to merge my expertise in DSA, competitive coding, and web development with emerging fields like machine learning and AI. Through continuous learning and innovation, I seek to make a meaningful impact on both the industry and society.
      </h2>
     </div>
     </>
